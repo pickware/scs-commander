506a7df31eb01a4f6c27f84fe276fdf633e008dc
Sven Münnich <sven.muennich@viison.com>
2016-11-01 11:39:36 +0100

    Prepare release of v0.2.0

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var Program = require('commander');
 
 // Define CLI
 Program
-    .version('0.0.1')
+    .version('0.2.0')
     .command('description', 'Updates the plugin description of a supported locale.')
     .command('upload', 'Uploads a plugin zip file and makes it available for download.')
     .command('list', 'Lists all available plugins.', {isDefault: true})
