906dd37247658232bef8894d540de5a8fc6c9f2b
Jannik Jochem <jannik.jochem@viison.com>
2017-02-13 16:51:11 +0100

    fix eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,20 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "plugins": [
-        "import"
-    ]
+  'extends': 'airbnb-base',
+  'plugins': [
+    'import'
+  ],
+  'rules': {
+    'no-console': 0,
+    'max-len': ['error', 120],
+    'no-param-reassign': 0,
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'never',
+    }],
+    'class-methods-use-this': 0,
+    'func-names': 0,
+  }
 };
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-'use strict';
+
 
 const Program = require('commander');
 
@@ -9,5 +9,5 @@ Program
     .version('0.4.0')
     .command('description', 'Updates the plugin description of a supported locale.')
     .command('upload', 'Uploads a plugin zip file and makes it available for download.')
-    .command('list', 'Lists all available plugins.', {isDefault: true})
+    .command('list', 'Lists all available plugins.', { isDefault: true })
     .parse(process.argv);
