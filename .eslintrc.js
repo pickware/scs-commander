6ede1a091b3f8b5cddd3f8d05cfd978d28f8f8ee
Felix Brucker <felix.brucker@live.de>
2017-04-18 10:42:47 +0200

    Fix ESLint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,5 @@ module.exports = {
         'indent': ['error', 4, { 'SwitchCase': 1 }],
         'no-use-before-define': ['error', { 'functions': false }],
         'filenames/match-regex': 2,
-        'filenames/match-exported': 2,
     }
 };
