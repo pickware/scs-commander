ad960c531b7a46da809a251d2025362c04997516
Sven Münnich <sven.muennich@viison.com>
2018-04-13 16:39:29 +0200

    Use 'viison-style-guide' for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,11 @@
 module.exports = {
-    'extends': 'airbnb-base',
-    'plugins': [
-        'import',
+    extends: "./node_modules/viison-style-guide/javascript/eslintrc.js",
+    plugins: [
         'filenames'
     ],
-    'rules': {
-        'no-console': 0,
-        'max-len': ['error', 120],
-        'no-param-reassign': 0,
-        'comma-dangle': ['error', {
-            arrays: 'always-multiline',
-            objects: 'always-multiline',
-            imports: 'always-multiline',
-            exports: 'always-multiline',
-            functions: 'never',
-        }],
-        'class-methods-use-this': 0,
-        'func-names': 0,
-        'newline-before-return': ['error'],
-        'indent': ['error', 4, { 'SwitchCase': 1 }],
-        'no-use-before-define': ['error', { 'functions': false }],
-        'filenames/match-regex': 2,
-    }
+    rules: {
+        'no-console': 'off',
+        'max-len': ['warn', 120],
+        'filenames/match-regex': 'error',
+    },
 };
